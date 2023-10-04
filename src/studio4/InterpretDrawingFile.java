89c91bbe9803aefcfd4abdc58cc3874942d62874
adam <adam@localhost>
2023-10-04 15:26:15 -0500

    111

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,7 @@
 package studio4;
 
+import java.awt.Color;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +21,43 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shape = in.next();
+		
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		int x = 0;
+		double[]newparameter = new double[x];
+		boolean fill = in.nextBoolean();
+		
+		if(shape == "ellipse") {
+			Color ellipse = new Color(red,green,blue);
+			StdDraw.setPenColor(ellipse);
+			double p1 = in.nextDouble();
+			double p2 = in.nextDouble();
+			double p3 = in.nextDouble();
+			double p4 = in.nextDouble();
+				if(fill == true) {
+					StdDraw.filledEllipse(p1,p2,p3,p4);
+			}else {
+				StdDraw.ellipse(p1,p2,p3,p4);
+			}
+		}else if(shape == "rectangle") {
+			Color rectangle = new Color(red,green,blue);
+			StdDraw.setPenColor(rectangle);
+			double p1 = in.nextDouble();
+			double p2 = in.nextDouble();
+			double p3 = in.nextDouble();
+			double p4 = in.nextDouble();
+			if(fill == true) {
+				StdDraw.filledRectangle(p1,p2,p3,p4);
+		}else {
+			StdDraw.rectangle(p1,p2,p3,p4);
+		}
+		}
+		
+		
 		
 	}
 }
